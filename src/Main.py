f38729d4003b0a9aa876862907b7bbb222535f6a
Alfredo Gallego <alfredogallegom@gmail.com>
2017-10-09 15:10:59 -0500

    Configuring main

--- src/Main.py
+++ src/Main.py
@@ -2,5 +2,19 @@
 
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+path = "~/Gutenberg/txt"
+
+if len(sys.argv) == 1:
+    print "Setting default path:", path
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-h":
+        print ("Compares and clusters documents, "
+               "[-p path] sets the path to a specified location")
+    sys.exit()
+
+if len(sys.argv) == 3:
+    if sys.argv[1] == "-p":
+        path = sys.argv[2]
+        print "The path is:", path
+
